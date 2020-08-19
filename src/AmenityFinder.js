0f2efc5d3f4a1d073dea3b38288a97f2793a1059
Peter Siska <psiska@inventage.com>
2020-08-19 18:03:30 +0200

    Add mwc-list as navigation

--- src/AmenityFinder.js
+++ src/AmenityFinder.js
@@ -2,6 +2,8 @@ import { LitElement, html, css } from 'lit-element';
 
 import '@material/mwc-drawer';
 import '@material/mwc-top-app-bar';
+import '@material/mwc-list/mwc-list.js';
+import '@material/mwc-list/mwc-list-item.js';
 
 export class AmenityFinder extends LitElement {
   static get styles() {
@@ -15,9 +17,12 @@ export class AmenityFinder extends LitElement {
   render() {
     return html`
       <mwc-drawer>
-        <div>
-          <p>Drawer Content!</p>
-        </div>
+        <mwc-list>
+          <mwc-list-item>Home</mwc-list-item>
+          <mwc-list-item>Search</mwc-list-item>
+          <mwc-list-item>Results</mwc-list-item>
+          <mwc-list-item>About</mwc-list-item>
+        </mwc-list>
         <div slot="appContent">
           <mwc-top-app-bar>
             <div slot="title">Title</div>
