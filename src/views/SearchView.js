64935c18c7014f27301e8884a56b94de6795c5a3
Peter Siska <psiska@inventage.com>
2020-08-24 10:28:29 +0200

    Add form fields

--- src/views/SearchView.js
+++ src/views/SearchView.js
@@ -5,7 +5,16 @@ import '@material/mwc-textfield';
 
 export class SearchView extends LitElement {
   render() {
-    return html`Search…`;
+    return html`
+      <h1>Search</h1>
+
+      <mwc-textfield label="Latitude"></mwc-textfield>
+      <mwc-textfield label="Longitude"></mwc-textfield>
+      <mwc-textfield label="Radius (m)"></mwc-textfield>
+
+      <mwc-button outlined label="Locate Me" icon="my_location"></mwc-button>
+      <mwc-button raised label="Search"></mwc-button>
+    `;
   }
 }
 
